2f6614e14d79fe28bcf63a2b783987fd2a2df03b
Jose D. Alvarez <jdalvarez23@gmail.com>
2020-02-21 00:04:24 -0600

    Update 2/20/2020 - Increased wait time..

--- src/js/main.js
+++ src/js/main.js
@@ -1776,7 +1776,7 @@ function insertFeelGoodAds() {
       timesRan++;
 
       // check if limit of times ran is met
-      if (timesRan == 100) {
+      if (timesRan == 200) {
         // stop the loop
         clearInterval(checkIfMediavineVideoExists);
         // reset the counter
@@ -2378,7 +2378,7 @@ function insertNonFeelGoodAds() {
       timesRan++;
 
       // check if limit of times ran is met
-      if (timesRan == 100) {
+      if (timesRan == 200) {
         // stop the loop
         clearInterval(checkIfMediavineVideoExists);
         // reset the counter
